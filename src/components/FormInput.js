378070d4fe58827ac872688edd0749d8e8d2667d
vinkims <vinkims@github.com>
2020-07-24 14:37:45 +0300

    Add FormInput component

---
+++ src/components/FormInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Stylesheet, Dimensions} from 'react-native';
+import {TextInput} from 'react-native-paper';
+
+const {width, height} = Dimensions.get('screen');
+
+export default function FormInput({labelName, ...rest}){
+    return(
+        <TextInput
+            label={labelName}
+            style={styles.input}
+            numberOfLines={1}
+            {...rest} // allows the component to have other prop values 
+        />
+    );
+}
+
+const styles = Stylesheet.create({
+    input:{
+        marginTop: 10,
+        marginBottom: 10,
+        width: width / 1.5,
+        height: height / 15
+    },
+});
