c09606068b6903d4cab61aebd6dd88737ca8cea8
vinkims <vinkims@github.com>
2020-08-11 16:25:15 +0300

    Create home screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {Title} from 'react-native-paper';
+import FormButton from '../components/FormButton';
+
+export default function HomeScreen(){
+    return(
+        <View style={styles.container}>
+            <Title>Home Screen</Title>
+            <Title>All chat rooms will be listed here</Title>
+            <FormButton modeValue='contained' title='Logout' />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#f5f5f5',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
